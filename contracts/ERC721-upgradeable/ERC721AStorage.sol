7de90976e251bb8dfc37df8caf5551aa2a3cf42d
Diego <Diegocortes079@gmail.com>
2022-07-29 00:57:34 -0500

    Add ERC721Storage and customize

---
+++ contracts/ERC721-upgradeable/ERC721AStorage.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+library ERC721AStorage {
+    // Reference type for token approval.
+    struct TokenApprovalRef {
+        address value;
+    }
+
+    struct Layout {
+        // =============================================================
+        //                            STORAGE
+        // =============================================================
+
+        // The next token ID to be minted.
+        uint256 _currentIndex;
+        // The number of tokens burned.
+        uint256 _burnCounter;
+        // Token name
+        string _name;
+        // Token symbol
+        string _symbol;
+        // Mapping from token ID to ownership details
+        // An empty struct value does not necessarily mean the token is unowned.
+        // See {_packedOwnershipOf} implementation for details.
+        //
+        // Bits Layout:
+        // - [0..159]   `addr`
+        // - [160..223] `startTimestamp`
+        // - [224]      `burned`
+        // - [225]      `nextInitialized`
+        // - [232..255] `extraData`
+        mapping(uint256 => uint256) _packedOwnerships;
+        // Mapping owner address to address data.
+        //
+        // Bits Layout:
+        // - [0..63]    `balance`
+        // - [64..127]  `numberMinted`
+        // - [128..191] `numberBurned`
+        // - [192..255] `aux`
+        mapping(address => uint256) _packedAddressData;
+        // Mapping from token ID to approved address.
+        mapping(uint256 => ERC721AStorage.TokenApprovalRef) _tokenApprovals;
+        // Mapping from owner to operator approvals
+        mapping(address => mapping(address => bool)) _operatorApprovals;
+
+        // Maximum number of mints allowed by transaction or wallet in presale
+        uint256 _maxBatchSizePreSale;
+
+        // Maximum number of mints allowed by transaction or wallet in publicSale
+        uint256 _maxBatchSizePublicSale;
+
+        // Maximum number of nfts that can be minted
+        uint256 _collectionSize;
+
+        // Maximum number of nfts that can be minted free
+        uint256 _amountForFree;
+
+        // Maximum number of ntfs that can be minted in presale.
+        uint256 _amountForPreSale;
+
+        // Maximum number of ntfs that can be minted in public sale.
+        uint256 _amountForPublicSale;
+
+        // Price by nft in public sale.
+        uint256 _pricePublicSale;
+
+        // Price by nft in presale.
+        uint256 _pricePreSale;
+
+        // Items sold in presale
+        uint256 _preSaleCurrentIndex;
+
+        // Items sold in  public sale
+        uint256 _publicSaleCurrentIndex;
+
+        // Items minted free
+        uint256 _freeSaleCurrentIndex;
+    }
+
+    bytes32 internal constant STORAGE_SLOT = keccak256('ERC721A.contracts.storage.ERC721A');
+
+    function layout() internal pure returns (Layout storage l) {
+        bytes32 slot = STORAGE_SLOT;
+        assembly {
+            l.slot := slot
+        }
+    }
+}
