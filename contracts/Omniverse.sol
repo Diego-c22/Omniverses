1eb204571b1a989c16eb023f96bba9518195dc10
Diego <Diegocortes079@gmail.com>
2022-07-29 19:16:33 -0500

    Add mint functions

--- contracts/Omniverse.sol
+++ contracts/Omniverse.sol
@@ -2,44 +2,78 @@
 
 pragma solidity ^0.8.4;
 
-import '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "./ERC721-upgradeable/ERC721AUpgradeable.sol";
 
 /** @author Diego Cortes **/
 /** @title Omniverse */
 contract Omniverse is ERC721AUpgradeable, OwnableUpgradeable {
-  uint256 private _maxPerAddressDuringMint;
-  uint256 private _amountForFree;
-  uint256 private _amountForPreSale;
-  uint256 private _amountForPublicSale;
+    function initialize() public initializerERC721A initializer {
+        __ERC721A_init({
+            name_: "Omniverses",
+            symbol_: "OMS",
+            pricePublicSale_: 0.05 ether,
+            pricePreSale_: 0.03 ether,
+            amountForFree_: 50,
+            amountForPreSale_: 500,
+            amountForPublicSale_: 450,
+            maxBatchSizePublicSale_: 10,
+            maxBatchSizePreSale_: 5
+        });
+        __Ownable_init();
+    }
 
-  function initialize(
-    uint256 maxBatchSize,
-    uint256 collectionSize,
-    uint256 amountForFree,
-    uint256 amountForPresale,
-    uint256 amountForPublicSale
-  ) initializerERC721A initializer public {
-    __ERC721A_init('Omniverses', 'OMS', maxBatchSize, collectionSize);
-    __Ownable_init();
-    _maxPerAddressDuringMint = maxBatchSize;
-    _amountForFree = amountForFree;
-    _amountForPreSale = amountForPresale;
-    _amountForPublicSale = amountForPublicSale;
-  }
+    function preSaleMint(uint256 quantity) external payable {
+        require(
+            ERC721AStorage.layout()._amountForPreSale <=
+                (ERC721AStorage.layout()._preSaleCurrentIndex + quantity),
+            "Transfer exceeds total suplay."
+        );
+        require(
+            balanceOf(msg.sender) + quantity <=
+                ERC721AStorage.layout()._maxBatchSizePreSale,
+            "Transfer exceeds max amount."
+        );
+        uint256 amount = quantity * ERC721AStorage.layout()._pricePreSale;
+        require(msg.value == amount, "Price not covered.");
+        _mint(msg.sender, quantity);
 
-  function preSaleMint(uint256 quantity) external payable {
-    _mint(msg.sender, quantity);
-  }
+        unchecked {
+            ERC721AStorage.layout()._preSaleCurrentIndex + quantity;
+        }
+    }
 
-  function publicSaleMint(uint256 quantity) external payable {
-    _mint(msg.sender, quantity);
-  }
+    function publicSaleMint(uint256 quantity) external payable {
+        require(
+            ERC721AStorage.layout()._amountForPublicSale <=
+                (ERC721AStorage.layout()._publicSaleCurrentIndex + quantity),
+            "Transfer exceeds total suplay."
+        );
+        require(
+            balanceOf(msg.sender) + quantity <=
+                ERC721AStorage.layout()._maxBatchSizePublicSale,
+            "Transfer exceeds max amount."
+        );
+        uint256 amount = quantity * ERC721AStorage.layout()._pricePublicSale;
+        require(msg.value == amount, "Price not covered.");
+        _mint(msg.sender, quantity);
 
-  function adminMint(uint256 quantity) external payable onlyOwner {
-    uint256 startTokenId = ERC721AStorage.layout()._currentIndex;
-    _mint(msg.sender, quantity);
-  }
+        unchecked {
+            ERC721AStorage.layout()._publicSaleCurrentIndex + quantity;
+        }
+    }
 
+    function freeMint(uint256 quantity) external payable {
+        require(
+            ERC721AStorage.layout()._amountForFree <=
+                (ERC721AStorage.layout()._freeSaleCurrentIndex + quantity),
+            "Transfer exceeds total suplay."
+        );
 
+        _mint(msg.sender, quantity);
+
+        unchecked {
+            ERC721AStorage.layout()._freeSaleCurrentIndex + quantity;
+        }
+    }
 }
